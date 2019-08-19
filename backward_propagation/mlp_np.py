1796a34b43fc16044fea6e48ff2bd381d8c9293c
Yujie Shui <shuiyujie.cn@gmail.com>
2019-08-20 07:53:00 +0800

    数理统计

---
+++ backward_propagation/mlp_np.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+describe: numpy 实现 BP 算法
+author:shui
+datetime:2019-08-18 23:11
+'''
+import tensorflow as tf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    
